f9cfed7063611be7b707fed1e158fc9045f77670
Dominick Del Bosque <dominick@theosfp.com>
2021-08-02 20:53:02 -0600

    update greet.rb

--- lib/greet.rb
+++ lib/greet.rb
@@ -2,5 +2,25 @@ require "greet/version"
 
 module Greet
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Greeter
+    # The greeter service definition.
+    class Service
+
+      include ::GRPC::GenericService
+
+      self.marshal_class_method = :encode
+      self.unmarshal_class_method = :decode
+      self.service_name = 'test.Greeter'
+
+      # Sends a greeting
+      rpc :SayHello, ::Test::HelloRequest, ::Test::HelloReply
+      # Sends a less formal greeting.
+      rpc :SayHi, ::Test::HelloRequest, ::Test::HelloReply
+      # return a number.
+      rpc :Number, ::Test::NumRequest, ::Test::NumberResponse
+    end
+
+    Stub = Service.rpc_stub_class
+  end
 end
